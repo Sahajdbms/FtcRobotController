6729f3a0d6001d2078932d03ba622384696f9d08
Sahajdbms <sahajdesai10@gmail.com>
2026-01-05 12:08:45 -0600

    fffffff

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTag.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTag.java
@@ -27,6 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import android.util.Size;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTags.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AprilTags {
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal visionPortal;
+    private List<AprilTagDetection> detectedTags = new ArrayList<>();
+    private Telemetry telemetry;
+    public void init (HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+    }
 }
