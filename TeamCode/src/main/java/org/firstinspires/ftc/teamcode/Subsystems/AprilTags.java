aec33758e7fc867c47fee5c57fb72ab84d44b26d
Yasha <yasha@wds4u.com>
2026-01-05 12:21:03 -0600

    Added basic subsystem for april tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/AprilTags.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -19,5 +25,23 @@ public class AprilTags {
         this.telemetry = telemetry;
 
         aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawCubeProjection(false)
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTagProcessor);
+
+        builder.build();
+    }
+
+    public void update() {
+        detectedTags = aprilTagProcessor.getDetections();
+    }
+    public List<AprilTagDetection> getDetectedTags() {
+        return detectedTags;
     }
 }
